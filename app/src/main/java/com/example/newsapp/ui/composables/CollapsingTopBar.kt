9fe2e16dea59607e7ef05b156a5369e460a928ec
Nazam <bun.nazam@gmail.com>
2023-04-18 18:23:17 +0200

    Commit message

--- app/src/main/java/com/example/newsapp/ui/composables/CollapsingTopBar.kt
+++ app/src/main/java/com/example/newsapp/ui/composables/CollapsingTopBar.kt
@@ -1,11 +1,14 @@
 package com.example.newsapp.ui.composables
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 
 @Composable
 fun CollapsingTopBar(
     title: @Composable () -> Unit,
-
+    onIconButtonClick: () -> Unit,
+    scrollState: LazyListState
 ) {
 
 
